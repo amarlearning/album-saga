5b7edd386fb9b267d0756089347ff937e6644659
Amar Prakash Pandey <amar.om1994@gmail.com>
2020-09-23 00:56:10 +0530

    Updates

---
+++ precache-manifest.1a1ac73ec41f8a54e3e233fd11ee8c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2574dbb90cbeac5bcadbb2f78fe30395",
+    "url": "/album-saga/index.html"
+  },
+  {
+    "revision": "6100756c026b7f737d41",
+    "url": "/album-saga/static/css/main.388aed35.chunk.css"
+  },
+  {
+    "revision": "4ee690f32bc19c876563",
+    "url": "/album-saga/static/js/2.9c685fcb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/album-saga/static/js/2.9c685fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6100756c026b7f737d41",
+    "url": "/album-saga/static/js/main.bf5afa79.chunk.js"
+  },
+  {
+    "revision": "bc65024ff61b58fbde56",
+    "url": "/album-saga/static/js/runtime-main.4969b8bc.js"
+  }
+]);
